439d440f9c0308ef029ea1bacb1953c0a2a00d6a
Ahmad El Lahib <ahmadellahib@gmail.com>
2020-12-08 09:27:41 +0100

    .

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -26,35 +26,6 @@ function action(event) {
   event.completed();
 }
 
-//event handling functions
-function onMessageComposeHandler(event) {
-  setSubject();  
-  // setSignature();
-  event.completed();
-}
-
-function onAppointmentComposeHandler(event) {
-  setSubject();
-  event.completed();
-}
-
-function setSubject() {
-  Office.context.mailbox.item.subject.setAsync("Set by an event-based add-in! 1232");
-}
-
-function setSignature() {
-  // Set the signature for the current item.
-  var signature = "This is my qm signature";
-  console.log(`Setting signature to "${signature}".`);
-  Office.context.mailbox.item.body.setSignatureAsync(signature, { coercionType: "html" },  function(asyncResult) {
-    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-      console.log("setSignatureAsync succeeded");
-    } else {
-      console.error(asyncResult.error);
-    }
-  });
-}
-
 function getGlobal() {
   return typeof self !== "undefined"
     ? self
@@ -69,7 +40,3 @@ const g = getGlobal();
 
 // the add-in command functions need to be available in global scope
 g.action = action;
-
-// bind event handling functions
-g.onMessageComposeHandler = onMessageComposeHandler;
-g.onAppointmentComposeHandler = onAppointmentComposeHandler;
